2d75531e7a823df84b6054f64c020033b71f6786
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-02-04 23:42:05 +0100

    fixed path and font size

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./HomePage"
-import GamePage from "./gamePage"
+import GamePage from "./GamePage"
 
 
 function App() {
