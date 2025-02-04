8516bdb11e659de7660dbe4e84f9c9c666c66e49
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-02-04 23:20:09 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./HomePage"
-import GamePage from "./GamePage"
+import GamePage from "./gamePage"
 
 
 function App() {
